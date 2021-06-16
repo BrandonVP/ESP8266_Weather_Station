3143cc537b2c23ddf0985ae5dc7bff83c74b5c93
Brandon Van Pelt <49537228+BrandonVP@users.noreply.github.com>
2021-06-16 12:13:01 -0500

    Update ESP8266_Weather_Station.ino

--- ESP8266_Weather_Station/ESP8266_Weather_Station.ino
+++ ESP8266_Weather_Station/ESP8266_Weather_Station.ino
@@ -35,9 +35,8 @@ LiquidCrystal_I2C lcd(I2C_ADDRESS, LCD_COLUMNS, LCD_ROWS);
 const char* ssid = "YOURWIFI";
 const char* password = "YOUPASSWORD";
 
-
 // Open Weather API URL and Key
-const String URL = "http://api.openweathermap.org/data/2.5/weather?lat=35.2497202&lon=-89.7660203&appid=12691a67a2a29e31a013778f0aac2778";
+const String URL = "Your Open Weather API Key";
 
 // Set true by system ticker
 bool getWeather = false;
